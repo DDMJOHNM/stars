5f489f3c6529ad62df7e564917d03f7daa765f14
John Mason <jmason176@gmail.com>
2020-11-08 13:46:51 +1300

    transitions fixed

--- public/js/script.js
+++ public/js/script.js
@@ -3,42 +3,24 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
 
-        //    console.log("1");
-        //    console.log(document.querySelector('.main'));
-        //    document.querySelector('.main').style.transition = "opacity 1s ease-in-out 2s";
-        //     document.querySelector('.main').style.opacity = 1;   
+    console.log("initial: " + location.href);
+    //first load page fade in    
+    document.querySelector('.main').style.transition = "opacity 1s ease-in-out 0.5s";
+            document.querySelector('.main').style.opacity = 1;   
+            document.querySelector('.conatiner .main').style.opacity = 1;   
+            clearInterval(interval);             
+        }, 200 );
 
-        
-            let els =  document.querySelectorAll('.nav-link');
-            els.forEach((el)=>{
-                
-                el.addEventListener("click",function(e){  
-                   // e.preventDefault()
-               
-                    // if(document.querySelector('main').style.opacity == 1){       console.log("2"); 
-                    //     document.querySelector('.main').style.transition = "opacity 1s ease-in-out 1s";
-                    //     document.querySelector('.main').style.opacity = 0;                        
-                    //     console.log(e.target.href)
-                    //     window.location.href === e.target.href;
-                    //     console.log(e.target.href)
-                    //     location.reload()   
-                    // } 
 
-                //     setInterval(()=>{
-                //         console.log("3");
-                //         if(document.querySelector('.main').style.opacity == 0){    
-                //         document.querySelector('.main').style.transition = "opacity 1s ease-in-out 1s";
-                //         document.querySelector('.main').style.opacity = 1;                        
-                //         console.log(e.target.href)
-                //         window.location.href === e.target.href;
-                //         console.log(e.target.href)
-                //         location.reload()   
-                //     } 
-                // },1000);
-              
-          })
+        let els =  document.querySelectorAll('.nav-link');
+        els.forEach((el)=>{
+            //on click reset loaction
+            el.addEventListener("click",function(e){  
+                location.assign(e.target.href);
+                location.reload();   
+            })
 
-    });
+        });
       
     
 });
--- themes/mytheme/static/js/script.js
+++ themes/mytheme/static/js/script.js
@@ -1,34 +1,30 @@
 
 //trigger page transitions global navigation
 
+ function pageDisplay(){
+    //first load page fade in    
+    document.querySelector('.main').style.transition = "opacity 1s ease-in-out 0.5s";
+    document.querySelector('.main').style.opacity = 1;   
+    document.querySelector('.main .container').style.opacity = 1;   
+    //clearInterval(interval);
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function(e){
 
-    console.log("initial: " + location.href);
-    //first load page fade in    
-    document.querySelector('.main').style.transition = "opacity 1s ease-in-out 1s";
-    document.querySelector('.main').style.opacity = 0;  
-  
-    console.log("page loaded -opacity 0" )
+        document.querySelector('.main').style.transition = "opacity 1s ease-in-out 0.5s";
+        document.querySelector('.main').style.opacity = 0;   
+        document.querySelector('.main .container').style.opacity = 0;   
 
-
-        var interval = setInterval(()=>{
-            console.log("page load set opacity back 1 ")                                                                  
-            document.querySelector('.main').style.transition = "opacity 1s ease-in-out 1s";
-            document.querySelector('.main').style.opacity = 1;   
-            document.querySelector('.conatiner .main').style.opacity = 1;   
-            clearInterval(interval);             
-        }, 500 );
-
+        let interval = setInterval(pageDisplay, 200 );
 
         let els =  document.querySelectorAll('.nav-link');
         els.forEach((el)=>{
-            //on click reset loaction
             el.addEventListener("click",function(e){  
-                location.assign(e.target.href);
-                location.reload();   
-            })
-
+            location.assign(e.target.href);
+            location.reload();   
+             })
         });
-      
     
 });
