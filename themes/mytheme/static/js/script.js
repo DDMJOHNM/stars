9a8632f19c5edfcd86cb0502fdc70cb3042038a0
John Mason <jmason176@gmail.com>
2020-11-08 15:47:50 +1300

    added css rotation for logo

--- themes/mytheme/static/js/script.js
+++ themes/mytheme/static/js/script.js
@@ -3,28 +3,34 @@
 
  function pageDisplay(){
     //first load page fade in    
-    document.querySelector('.main').style.transition = "opacity 1s ease-in-out 0.5s";
+   
     document.querySelector('.main').style.opacity = 1;   
-    document.querySelector('.main .container').style.opacity = 1;   
+    document.querySelector('.main .container').style.opacity = 1;  
     //clearInterval(interval);
 }
 
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
-        document.querySelector('.main').style.transition = "opacity 1s ease-in-out 0.5s";
+       
+        document.querySelector('.logo').classList.add('rotate');  
         document.querySelector('.main').style.opacity = 0;   
-        document.querySelector('.main .container').style.opacity = 0;   
-
-        let interval = setInterval(pageDisplay, 200 );
+        document.querySelector('.main .container').style.opacity = 0;          
+        document.querySelector('.main').style.transition = "opacity 1s ease-in-out 0.5s";    
+        // document.querySelector('.logo').style.animation = "rotation 5s infinite linear";       
+        // document.querySelector('.logo').style.animationPlayState = "running"; 
+                
+        let interval = setInterval(pageDisplay, 1500 );
 
         let els =  document.querySelectorAll('.nav-link');
         els.forEach((el)=>{
-            el.addEventListener("click",function(e){  
-            location.assign(e.target.href);
-            location.reload();   
-             })
+            el.addEventListener("click",function(e){
+                document.querySelector('.logo').classList.remove('rotate');                  
+                location.assign(e.target.href);            
+                location.reload();   
+             }) 
+           
+             
         });
     
 });
