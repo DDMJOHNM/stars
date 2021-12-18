b615bae6c3be7d4b6ddcd4e549424751ee329e12
John Mason <jmason176@gmail.com>
2021-12-19 12:30:54 +1300

    dec2021 changes

--- content/shows/21-1-2022-Gathering-Shadows/index.md
+++ content/shows/21-1-2022-Gathering-Shadows/index.md
@@ -1,6 +1,6 @@
 ---
 title: "22/1/2022 Gathering Shadows, Valhalla, Wellington"
-date: 2022-01-21T00:51:16+13:00
+date: 2021-12-18T00:51:16+13:00
 draft: false
 ---
 
---
+++ serve/js/script.js
@@ -0,0 +1,36 @@
+
+//trigger page transitions global navigation
+
+ function pageDisplay(){
+    //first load page fade in    
+   
+    document.querySelector('.main').style.opacity = 1;   
+    document.querySelector('.main .container').style.opacity = 1;  
+    //clearInterval(interval);
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+       
+        document.querySelector('.logo').classList.add('rotate');  
+        document.querySelector('.main').style.opacity = 0;   
+        document.querySelector('.main .container').style.opacity = 0;          
+        document.querySelector('.main').style.transition = "opacity 0.5s ease-in-out 0.5s";    
+        // document.querySelector('.logo').style.animation = "rotation 5s infinite linear";       
+        // document.querySelector('.logo').style.animationPlayState = "running"; 
+                
+        let interval = setInterval(pageDisplay, 200 );
+
+        let els =  document.querySelectorAll('.nav-link');
+        els.forEach((el)=>{
+            el.addEventListener("click",function(e){
+                document.querySelector('.logo').classList.remove('rotate');                  
+                location.assign(e.target.href);            
+                location.reload();   
+             }) 
+           
+             
+        });
+    
+});
